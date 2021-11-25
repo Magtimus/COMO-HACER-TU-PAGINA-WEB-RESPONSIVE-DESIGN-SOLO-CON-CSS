645e5285433129ef50b4953fb674a2257ec0ab70
Magtimus <cristiancedano1030@gmail.com>
2021-11-25 09:16:00 -0400

    COMO HACER TU PÁGINA WEB RESPONSIVE DESIGN SOLO CON CSS

---
+++ js/script.js
@@ -0,0 +1,19 @@
+document.getElementById("btn_menu").addEventListener("click", mostrar_menu);
+
+document.getElementById("back_menu").addEventListener("click", ocultar_menu);
+
+nav = document.getElementById("nav");
+background_menu = document.getElementById("back_menu");
+
+function mostrar_menu(){
+
+    nav.style.right = "0px";
+    background_menu.style.display = "block";
+}
+
+function ocultar_menu(){
+
+    nav.style.right = "-250px";
+    background_menu.style.display = "none";
+}
+
